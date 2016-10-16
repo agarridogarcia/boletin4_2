0d8d32ae5141156b2c0785faa8edc9f6885a9e0b
Ara <Ara@192.168.0.13>
2016-10-16 14:30:51 +0200

    satelite

---
+++ src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,14 @@
+
+package boletin4_2;
+
+
+public class Boletin4_2 {
+
+    
+    public static void main(String[] args) {
+        Satelite Satelite1= new Satelite();
+        Satelite1.Satelite(40, 20, 10000);
+        Satelite1.verPosicion();
+    }
+    
+}
